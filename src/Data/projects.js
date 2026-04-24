63c2e0bfc19279f9b57cb4d6c849d954c87be098
Eyob Alayu <eyobalayu15@gmail.com>
2026-04-24 17:12:27 +0300

    .

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -4,7 +4,7 @@ export const projects = [
     title: "Food Delivery",
     description:
       "I built a full-stack food delivery web app where users can easily browse menus, add items to their cart, and place orders with secure online payments. It also includes an admin dashboard to manage orders, users, and menu items. ",
-    image: "/assets/food_delivery.png",
+    image: "src/assets/food_delivery.png",
     category: "Full Stack",
     techs: ["React", "Node.js", "MongoDB", " Stripe"],
     liveURL: "https://eyobas-food-delivery.vercel.app/",
@@ -15,7 +15,7 @@ export const projects = [
     title: "My Portfolio",
     description:
       "A modern and responsive portfolio website built using React and Tailwind CSS to showcase my projects, skills, and experience. The application focuses on clean design, smooth user experience, and mobile-friendly layouts.",
-    image: "/src/assets/portfolio.png",
+    image: "src/assets/portfolio.png",
     category: "Full Stack",
     techs: ["React", "tailwind", "mailjs"],
     liveURL: "https://eyobas-portfolio.vercel.app/",
