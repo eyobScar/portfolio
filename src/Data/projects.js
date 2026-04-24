a6297bed5e84f4efb4de2d440fdf0c9d81bd4791
Eyob Alayu <eyobalayu15@gmail.com>
2026-04-24 16:58:59 +0300

    new project added

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -7,19 +7,19 @@ export const projects = [
     image: "/assets/food_delivery.png",
     category: "Full Stack",
     techs: ["React", "Node.js", "MongoDB", " Stripe"],
-    liveURL: "https://",
+    liveURL: "https://eyobas-food-delivery.vercel.app/",
     githubURL: "https://github.com/eyobScar/eyob-portfolio.git",
   },
   {
     id: 2,
-    title: "Food Delivery",
+    title: "My Portfolio",
     description:
-      "I built a full-stack food delivery web app where users can easily browse menus, add items to their cart, and place orders with secure online payments. It also includes an admin dashboard to manage orders, users, and menu items. ",
-    image: "/assets/food_delivery.png",
+      "A modern and responsive portfolio website built using React and Tailwind CSS to showcase my projects, skills, and experience. The application focuses on clean design, smooth user experience, and mobile-friendly layouts.",
+    image: "/assets/portfolio.png",
     category: "Full Stack",
-    techs: ["React", "Node.js", "MongoDB", " Stripe"],
-    liveURL: "https://",
-    githubURL: "https://github.com",
+    techs: ["React", "tailwind", "mailjs"],
+    liveURL: "https://eyobas-portfolio.vercel.app/",
+    githubURL: "https://github.com/eyobScar/portfolio",
   },
   {
     id: 3,
@@ -32,5 +32,4 @@ export const projects = [
     liveURL: "https://",
     githubURL: "https://github.com",
   },
-
 ];
